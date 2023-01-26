6e636e5c32be241b3d94d7d7243b3f34176c4e2a
vduernberger <100835083+vduernberger@users.noreply.github.com>
2023-01-26 10:28:23 +0100

    Bowling

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Manager : MonoBehaviour
 {
     public GameObject[] kegel;
     public GameObject playingSphere;
+    public TMP_Text descriptionText;
+    public static int kegelCount;
+    //public bool kegelStatus;
     
+
 
     // Start is called before the first frame update
     void Start()
     {
-       // OnCollisionEnter(playingSphere);
+        descriptionText.text = "Wellcome to bowling!";
+        kegelCount = 0;
+        //kegelStatus = gameObject.GetComponent<Kegel>();
+        //Debug.Log(kegelStatus);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "kegel")
-        {
-            Debug.Log("KEGELLLLL !!!");
-        }
-        Debug.Log("Es geht generell!!!"); 
-    }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        /*if (kegel[0].transform.rotation.z >= 0.45)
+        {
+            kegelCount++;
+            Debug.Log("first one fallen");
+        }
+        */
+        /*if(kegelStatus == true)
+        {
+            kegelCount++;
+        }*/
+
+       /*if(kegel[0].kegelFallen == true)
+        {
+            kegelCount++;
+        }*/
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            descriptionText.text = "Congrats! You scored " + kegelCount + " Points";
+            Debug.Log(kegelCount);
+        }
+
     }
 }
---
+++ Assets/MyGame/Scripts/PlayingSphere.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingSphere : MonoBehaviour
+{
+    public GameObject playingSphere;
+    public Rigidbody rb;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playingSphere = gameObject.Getcomponent();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "kegel")
+        {
+            Debug.Log("touched a Kegel");
+
+        }
+        // Debug.Log("Es geht generell");
+        if (collision.gameObject.tag == "grenze")
+        {
+            Debug.Log("outside");
+            Destroy(gameObject);
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+        
+    }
+}
+
